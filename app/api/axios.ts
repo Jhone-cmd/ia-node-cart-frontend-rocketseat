ccde4056e4fe84ee4e7b0d762ee56bb58f01f146
jhone-cmd <jhone.1941@gmail.com>
2025-12-11 09:55:23 -0300

    installing shadcn and its components

--- app/api/axios.ts
+++ app/api/axios.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { Product } from '../types/product'
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -11,5 +12,5 @@ export const getCatalog = async (search = '') => {
   const response = await api.get('/catalog', {
     params: { search },
   })
-  return response.data
+  return response.data as Product[]
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 'use client'
-import useSWR from 'swr'
-import { getCatalog } from './api/axios'
 
 export default function Home() {
-  const products = useSWR('/api/catalog', async () => getCatalog())
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
-      <h1>Product Catalog</h1>
-      <pre>{JSON.stringify(products.data, null, 2)}</pre>
+      Em Breve
     </div>
   )
 }
