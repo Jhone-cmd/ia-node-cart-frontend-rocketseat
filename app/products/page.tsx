8eb439e9880c266467e75c133ffe1d385d31afc1
jhone-cmd <jhone.1941@gmail.com>
2025-12-11 10:42:47 -0300

    adjusting the product page to search for products using the search bar.

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,14 +1,30 @@
 'use client'
 
+import { useSearchParams } from 'next/navigation'
 import useSWR from 'swr'
+import { Input } from '@/components/ui/input'
 import { getCatalog } from '../api/axios'
 
 export default function ProductsPage() {
-  const products = useSWR('/api/catalog', async () => getCatalog())
+  const searchParams = useSearchParams()
+  const search = searchParams.get('search') || ''
+  const products = useSWR(`/api/catalog?q=${search}`, async () =>
+    getCatalog(search)
+  )
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    window.history.pushState({}, '', `/products?search=${e.target.value}`)
+  }
 
   return (
-    <div>
-      <pre>{JSON.stringify(products.data, null, 2)}</pre>
+    <div className="my-10">
+      <Input onChange={handleSearch} />
+      {products.data?.map(product => (
+        <div className="border-b p-4" key={product.id}>
+          <h2 className="font-semibold text-lg">{product.name}</h2>
+          <p className="text-green-600">{(product.price / 100).toFixed(2)}</p>
+        </div>
+      ))}
     </div>
   )
 }
