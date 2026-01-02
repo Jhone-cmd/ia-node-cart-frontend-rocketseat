a9aa82553c77677d755500e0ef6ab4d4d1adc112
jhone-cmd <jhone.1941@gmail.com>
2026-01-02 14:53:10 -0300

    add the routes that will be used in the chat.

--- app/api/axios.ts
+++ app/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
-import { Product } from '../types/product'
 import { Cart } from '../types/cart'
+import { Product } from '../types/product'
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -16,22 +16,81 @@ export const getCatalog = async (search = '') => {
   return response.data as Product[]
 }
 
-
 export const getCart = async () => {
-  const response = await api.get('/cart');
-  return response.data as Cart;
+  const response = await api.get('/cart')
+  return response.data as Cart
 }
 
 export const addToCart = async (productId: number, quantity: number) => {
   const response = await api.post('/cart', { productId, quantity })
-  return response.data as { id: number};
+  return response.data as { id: number }
 }
 
-export const updateCartItemQuantity = async (cartId: number, productId: number, quantity: number) => {
-  const response = await api.put(`/cart/${cartId}/items/${productId}`, { quantity })
-  return response.data as Cart;
+export const updateCartItemQuantity = async (
+  cartId: number,
+  productId: number,
+  quantity: number
+) => {
+  const response = await api.put(`/cart/${cartId}/items/${productId}`, {
+    quantity,
+  })
+  return response.data as Cart
 }
 
 export const deleteCartItem = async (cartId: number, productId: number) => {
   await api.delete(`/cart/${cartId}/items/${productId}`)
+}
+
+export const getChatSessions = async () => {
+  const response = await api.get('/chat')
+  return response.data
+}
+
+export const getChatSession = async (sessionId: number) => {
+  const response = await api.get(`/chat/${sessionId}`)
+  return response.data
+}
+
+export const createChatSession = async () => {
+  try {
+    const response = await api.post<{ id: number }>('/chat')
+
+    return response.data
+  } catch (error) {
+    console.error('Error creating chat session:', error)
+    return null
+  }
+}
+
+export const sendMessageToChat = async (sessionId: number, message: string) => {
+  try {
+    const response = await api.post(`/chat/${sessionId}/messages`, {
+      content: message,
+    })
+    return response.data
+  } catch (error) {
+    console.error('Error sending message to chat:', error)
+    return null
+  }
+}
+
+export const chooseCartFromComparison = async (cartId: number) => {
+  try {
+    await api.post(`/chat/${cartId}/choose`)
+  } catch (error) {
+    console.error('Error choosing cart from comparison:', error)
+    return null
+  }
+}
+
+export const confirmAction = async (actionId: number, sessionId: number) => {
+  try {
+    const response = await api.post(
+      `/chat/${sessionId}/actions/${actionId}/confirm`
+    )
+    return response.data
+  } catch (error) {
+    console.error('Error confirming action:', error)
+    return null
+  }
 }
